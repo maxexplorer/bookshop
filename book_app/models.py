a4a85ed10988f9e0c81f46869c2ea4c094eda7e2
maxexplorer <maxexplorer@mail.ru>
2022-03-06 13:05:24 +0300

    Add column in models.py

--- book_app/models.py
+++ book_app/models.py
@@ -6,6 +6,8 @@ class Book(models.Model):
 
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
+    is_best_selling = models.BooleanField()
+    author = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f'Title: {self.title}, rating: {self.rating}'
