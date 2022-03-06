a374d2c6f025764e86f2813ef4acbfce19770af6
maxexplorer <maxexplorer@mail.ru>
2022-03-06 06:55:50 +0300

    Add magic method __str__ in models.py

--- book_app/models.py
+++ book_app/models.py
@@ -7,5 +7,9 @@ class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
 
+    def __str__(self):
+        return f'Title: {self.title}, rating: {self.rating}'
+
+
 
 
