efe9ac4253da6d153c8762160060a3cc48a33675
maxexplorer <maxexplorer@mail.ru>
2022-03-08 15:18:06 +0300

    Add URLconf, views and templates HTML

--- book_app/urls.py
+++ book_app/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('book_app.urls')),
+    path('', views.show_all_books),
+
 ]
--- book_app/views.py
+++ book_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def f():
-    pass
+def show_all_books(request):
+    render(request, 'book_app/all_books.html')
