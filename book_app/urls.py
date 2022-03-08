36ab2d9901f4d94c7a4d2963e676e4dcf87f2426
maxexplorer <maxexplorer@mail.ru>
2022-03-08 15:58:59 +0300

    views.py and all_books.html

--- book_app/urls.py
+++ book_app/urls.py
@@ -3,5 +3,4 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.show_all_books),
-
 ]
--- book_app/views.py
+++ book_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 
 def show_all_books(request):
-    render(request, 'book_app/all_books.html')
+    books = Book.objects.all()
+    data = {
+        'books': books
+    }
+    return render(request, 'book_app/all_books.html', context=data)
