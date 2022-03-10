584f7c803af3ecf5236bfd83079645b9532bea59
maxexplorer <maxexplorer@mail.ru>
2022-03-10 20:05:50 +0300

    Add method get_url

--- book_app/models.py
+++ book_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -8,6 +9,9 @@ class Book(models.Model):
     rating = models.IntegerField()
     is_best_selling = models.BooleanField(null=True)
     author = models.CharField(max_length=100, null=True)
+
+    def get_url(self):
+        return reverse('book_detail', args=[self.id])
 
     def __str__(self):
         return f'Title: {self.title}, rating: {self.rating}, author: {self.author}'
